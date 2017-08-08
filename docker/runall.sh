28cdd0448d7912d3856f17163607e31824430ffe
mohsaif <msaif27@gmail.com>
2017-08-08 16:03:37 +0300

    Update runall.sh

--- docker/runall.sh
+++ docker/runall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set  -e
 # Stop & Remove All Dockers
-for inst in `docker ps -a | awk '{print $1}' | egrep -v 'd5c9e5830be8|CONTAINER'`
+for inst in `docker ps -a | awk '{print $1}' | egrep -v '2161009001df|CONTAINER'|egrep -v '0efd18157cae|CONTAINER'`
    do
       echo working with $inst
       docker stop $inst
@@ -12,15 +12,22 @@ for inst in `docker ps -a | awk '{print $1}' | egrep -v 'd5c9e5830be8|CONTAINER'
  done
 
 # Build & Run All Dockers
-cd /home/jenkins/workspace/RC-DOCKER/docker
+cd /home/jenkins/workspace/Docker-Registry/docker
 for dfile in `ls Dockerfile.*`
  do
       osname=`echo $dfile | awk -F\. '{print $2}'`
       echo building $osname
       docker build -t $osname --file $dfile .
       [ $? -ne 0 ] && echo something went wrong && continue
-      echo running $osname
+#     echo running $osname
 #      docker run -dit $osname /bin/bash
 #      echo
  done
+for dfile in `ls Dockerfile.*`
+ do
+      osname=`echo $dfile | awk -F\. '{print $2}'`
 
+        docker tag $osname 10.254.154.110/$osname
+        docker push 10.254.154.110/$osname
+
+done
