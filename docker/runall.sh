d6b8a1836ea6c24524516f0df43b4f02ce7163be
mohsaif <msaif27@gmail.com>
2017-08-09 13:59:46 +0300

    Update runall.sh

--- docker/runall.sh
+++ docker/runall.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set  -e
 # Stop & Remove All Dockers
+id
 chmod o=rw /var/run/docker.sock
 for inst in `docker ps -a | awk '{print $1}' | egrep -v '2161009001df|CONTAINER'|egrep -v '0efd18157cae|CONTAINER'`
    do
