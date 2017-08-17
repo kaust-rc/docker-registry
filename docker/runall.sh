edbd20e08b2373e3845bea307ca406732ca36b7e
Antonio <antonio.arena@kaust.edu.sa>
2017-08-17 15:59:32 +0300

    Reformatting of BASH script.

--- docker/runall.sh
+++ docker/runall.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 set  -e
+
 # Stop & Remove All Dockers
 id
 sudo chmod o=rw /var/run/docker.sock
+
 for inst in `docker ps -a | awk '{print $1}' | egrep -v '2161009001df|CONTAINER'|egrep -v '0efd18157cae|CONTAINER'`
-   do
-      echo working with $inst
-      docker stop $inst
-      [ $? -ne 0 ] && echo something went wrong with $inst && continue
-      docker rm $inst
-      echo
-      echo
+do
+    echo working with $inst
+    docker stop $inst
+    [ $? -ne 0 ] && echo something went wrong with $inst && continue
+    docker rm $inst
+    echo
+    echo
  done
 
 # Build & Run All Dockers
 cd /home/jenkins/workspace/Docker-Registry/docker
 for dfile in `ls Dockerfile.*`
- do
-      osname=`echo $dfile | awk -F\. '{print $2}'`
-      echo building $osname
-      docker build -t $osname --file $dfile .
-      [ $? -ne 0 ] && echo something went wrong && continue
-      docker tag $osname 10.254.154.110/$osname
-      docker push 10.254.154.110/$osname
-#     echo running $osname
-#      docker run -dit $osname /bin/bash
-#      echo
+do
+    osname=`echo $dfile | awk -F\. '{print $2}'`
+    echo building $osname
+    docker build -t $osname --file $dfile .
+    [ $? -ne 0 ] && echo something went wrong && continue
+    docker tag $osname 10.254.154.110/$osname
+    docker push 10.254.154.110/$osname
 done
-
-#for dfile in `ls Dockerfile.*`
-#do
-#      osname=`echo $dfile | awk -F\. '{print $2}'`
-#
-#        docker tag $osname 10.254.154.110/$osname
-#        docker push 10.254.154.110/$osname
-#
-#done
